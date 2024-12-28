fa46c6d487444d24c6179b0993d384cd92705e8c
EmmaAmos <elovestodraw@gmail.com>
2024-12-28 13:48:10 -0500

    Adding a few things to the US side

--- nav.js
+++ nav.js
@@ -5,7 +5,7 @@
             document.getElementById('nav-placeholder').innerHTML = data;
         });
 
-        fetch('Social.html')
+    fetch('Social.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('endCard').innerHTML = data;
