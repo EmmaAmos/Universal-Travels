716fd0e556940aeb7de7f66ce983aafab4522944
ea0120 <amos21005@byu1.edu>
2025-01-04 10:06:16 -0500

    adding social, plus a headline id

--- nav.js
+++ nav.js
@@ -1,11 +1,16 @@
 //Nav Fetch
-    fetch('Nav.html')
+    fetch('/Nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('nav-placeholder').innerHTML = data;
         });
+        fetch('/Headline.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('headline').innerHTML = data;
+        });
 
-    fetch('Social.html')
+    fetch('/Social.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('endCard').innerHTML = data;
